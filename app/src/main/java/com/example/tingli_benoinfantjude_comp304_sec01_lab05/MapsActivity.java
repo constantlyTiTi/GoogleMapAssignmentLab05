b47b1f4cf54fb58bf63dcec34078ddefa4d443b6
Ting <mycentry1986@gmail.com>
2020-08-09 15:45:11 -0400

    first commit

---
+++ app/src/main/java/com/example/tingli_benoinfantjude_comp304_sec01_lab05/MapsActivity.java
@@ -0,0 +1,287 @@
+package com.example.tingli_benoinfantjude_comp304_sec01_lab05;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static androidx.core.content.PermissionChecker.PERMISSION_GRANTED;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private Polyline mPolyline;
+    private static final int LOCATION_PERMISSION_REQUEST = 1;
+    private static final int REQUEST_CHECK_SETTINGS = 2;
+    private static final String ERROR_MSG = "Google Play services are unavailable.";
+
+    private int mapType;
+    private LocationRequest mLocationRequest;
+    private List<Marker> mMarkers= new ArrayList<>();
+
+    LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location location = locationResult.getLastLocation();
+            if (location != null) {
+                if (mMap != null) {
+                    LatLng latLng = new LatLng(location.getLatitude(),
+                            location.getLongitude());
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                    Calendar c = Calendar.getInstance();
+                    String dateTime = DateFormat.format("MM/dd/yyyy HH:mm:ss",
+                            c.getTime()).toString();
+
+                    int markerNumber = mMarkers.size() + 1;
+                    mMarkers.add(mMap.addMarker(new MarkerOptions()
+                            .position(latLng)
+                            .title(dateTime)
+                            .snippet("Marker #" + markerNumber +
+                                    " @ " + dateTime)));
+
+                    List<LatLng> points = mPolyline.getPoints();
+                    points.add(latLng);
+                    mPolyline.setPoints(points);
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        GoogleApiAvailability availability = GoogleApiAvailability.getInstance();
+
+        int result = availability.isGooglePlayServicesAvailable(this);
+        if (result != ConnectionResult.SUCCESS) {
+            if (!availability.isUserResolvableError(result)) {
+                Toast.makeText(this, ERROR_MSG, Toast.LENGTH_LONG).show();
+            }
+        }
+
+        mLocationRequest = new LocationRequest()
+                .setInterval(5000)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if we have permission to access high accuracy fine location.
+        int permission = ActivityCompat.checkSelfPermission(this,
+                ACCESS_FINE_LOCATION);
+
+        // If permission is granted, fetch the last location.
+        if (permission == PERMISSION_GRANTED) {
+            getLastLocation();
+        } else {
+            // If permission has not been granted, request permission.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST);
+        }
+
+        // Check of the location settings are compatible with our Location Request.
+        LocationSettingsRequest.Builder builder =
+                new LocationSettingsRequest.Builder()
+                        .addLocationRequest(mLocationRequest);
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+        task.addOnSuccessListener(this,
+                new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse
+                                                  locationSettingsResponse) {
+                        // Location settings satisfy the requirements of the Location Request.
+                        // Request location updates.
+                        requestLocationUpdates();
+                    }
+                });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // Extract the status code for the failure from within the Exception.
+                int statusCode = ((ApiException) e).getStatusCode();
+
+                switch (statusCode) {
+                    case CommonStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            // Display a user dialog to resolve the location settings
+                            // issue.
+                            ResolvableApiException resolvable = (ResolvableApiException) e;
+                            resolvable.startResolutionForResult(MapsActivity.this,
+                                    REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException sendEx) {
+                            Log.e("Lab05", "Location Settings resolution failed.", sendEx);
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings issues can't be resolved by user.
+                        // Request location updates anyway.
+                        Log.d("Lab05", "Location Settings can't be resolved.");
+                        requestLocationUpdates();
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
+
+        PolylineOptions polylineOptions = new PolylineOptions()
+                .color(Color.GREEN)
+                .geodesic(true);
+        mPolyline = mMap.addPolyline(polylineOptions);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST) {
+            if (grantResults[0] != PERMISSION_GRANTED)
+                Toast.makeText(this, "Location Permission Denied",
+                        Toast.LENGTH_LONG).show();
+            else
+                getLastLocation();
+        }
+    }
+
+    private void getLastLocation() {
+        FusedLocationProviderClient fusedLocationClient;
+        fusedLocationClient =
+                LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)
+                == PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION)
+                        == PERMISSION_GRANTED) {
+            fusedLocationClient.getLastLocation();
+//                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+//                        @Override
+//                        public void onSuccess(Location location) {
+////                            updateTextView(location);
+//                        }
+//                    });
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.map_type,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.menu_normal:
+                mapType=GoogleMap.MAP_TYPE_NORMAL;
+                mMap.setMapType(mapType);
+                break;
+            case  R.id.menu_hybrid:
+                mapType=GoogleMap.MAP_TYPE_HYBRID;
+                mMap.setMapType(mapType);
+                break;
+            case  R.id.menu_satellite:
+                mapType=GoogleMap.MAP_TYPE_SATELLITE;
+                mMap.setMapType(mapType);
+                break;
+            case  R.id.menu_terrain:
+                mapType=GoogleMap.MAP_TYPE_TERRAIN;
+                mMap.setMapType(mapType);
+                break;
+            default:
+                mapType=GoogleMap.MAP_TYPE_NORMAL;
+                mMap.setMapType(mapType);
+                return super.onOptionsItemSelected(item);
+
+        }
+        return true;
+    }
+
+    private void requestLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)
+                == PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION)
+                        == PERMISSION_GRANTED) {
+
+            FusedLocationProviderClient fusedLocationClient
+                    = LocationServices.getFusedLocationProviderClient(this);
+
+            fusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+        }
+    }
+}
